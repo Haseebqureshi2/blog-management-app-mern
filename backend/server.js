4f1949bde902d58006f590fd5b96de627191c7ae
Haseeb Qureshi <haseebqureshi5152@gmail.com>
2026-01-09 21:23:36 +0500

    Configure CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
