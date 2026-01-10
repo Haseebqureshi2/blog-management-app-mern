1edc314778608d290e4f975c17c10e26e36d3f3b
Haseeb Qureshi <haseebqureshi5152@gmail.com>
2026-01-10 14:01:22 +0500

    Update API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://blog-management-app-mern-production.up.railway.app/"
 });
 
 API.interceptors.request.use((req) => {
